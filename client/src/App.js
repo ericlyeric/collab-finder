2e302e0aed94b29b417bfcdfe2fd065726730178
ericlyeric <ericlyeric@gmail.com>
2021-06-10 22:17:10 -0700

    add client folder tracking

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+function App() {
+    return (
+        <div className="App">
+            <div
+                class="fb-login-button"
+                data-width=""
+                data-size="large"
+                data-button-type="continue_with"
+                data-layout="default"
+                data-auto-logout-link="false"
+                data-use-continue-as="false"
+            ></div>
+        </div>
+    );
+}
+
+export default App;
